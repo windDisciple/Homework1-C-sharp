4a26b57feccd253676df3867bc784050d73a72d8
windDisciple <tellittotfishes@gmail.com>
2022-12-05 21:59:43 +0300

    Task6 completed

---
+++ Task6.cs
@@ -0,0 +1,8 @@
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0) {
+    System.Console.WriteLine("Число четное");
+} else {
+    System.Console.WriteLine("Число нечетное");
+}
