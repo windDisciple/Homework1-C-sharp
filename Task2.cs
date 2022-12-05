a2d0f632555a76fa38dbd58179aaba203efc293b
windDisciple <tellittotfishes@gmail.com>
2022-12-05 21:49:19 +0300

    Task2 complete

--- Task2.cs
+++ Task2.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) {
+    System.Console.WriteLine("max = " + num1);
+} else {
+    System.Console.WriteLine("max = " + num2);
+}
