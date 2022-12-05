845ec7417fc5caa975f7117e5a9c8ee1583b0c5c
windDisciple <tellittotfishes@gmail.com>
2022-12-05 21:58:23 +0300

    Task4 completed

---
+++ Task4.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c) {
+    System.Console.WriteLine("Max = " + a);
+} else if (a > b && a < c) {
+    System.Console.WriteLine("Max = " + c);
+} else if (a < b && c < b) {
+    System.Console.WriteLine("Max = " + b);
+}
